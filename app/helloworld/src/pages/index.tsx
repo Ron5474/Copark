1a8ad079ed2c3f075a846033b84fb8894404c46f
Atharva Darshan Tawde <atawde@ucsc.edu>
2025-04-24 18:04:51 -0700

    helloworld v1

---
+++ app/helloworld/src/pages/index.tsx
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import { Geist, Geist_Mono } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Hello World!</title>
+      </Head>
+      <div
+        className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
+      >
+        <h1>Hello World!</h1>
+        <h2>Contibutors</h2>
+        <p>Atharva Tawde</p>
+      </div>
+    </>
+  );
+}

9e4920021e8dc620bbb6277a8797832ae854cf65
Bryant Oliver <bcoliver@ucsc.edu>
2025-05-02 01:00:42 -0700

    driver addVehicle change country/state

--- app/driver/src/app/addVehicle/Form.tsx
+++ app/driver/src/app/addVehicle/Form.tsx
@@ -11,15 +11,12 @@
 
 'use client'
 
-import { Fragment, useState, useEffect } from 'react'
-import { useRouter } from 'next/navigation'
+import { useState, useEffect } from 'react'
 import {
   Box,
   Typography,
   TextField,
-  FormGroup,
-  FormControlLabel,
-  Checkbox,
+  MenuItem,
   Button
  } from '@mui/material'
 
@@ -39,10 +36,16 @@ import {
 export default function View() {
   const [plateNumber, setPlateNumber] = useState<string>('')
   const [isValidEntry, setIsValidEntry] = useState<boolean>(true)
+  const [country, setCountry] = useState<string>(Object.keys(locations)[0])
+  const [state, setState] = useState<string>(Object.values(locations)[0][0])
 
   const submitVehicle = () => {
     setIsValidEntry(plateNumber.length > 0)
   }
+
+  useEffect(() => {
+    setState(locations[country][0])
+  }, [country])
 
   const textFieldStyle = {
     margin: 0,
@@ -106,6 +109,46 @@ export default function View() {
           onChange={(event) => setPlateNumber(event.target.value)}
         />
       </Box>
+      <Box sx={{marginTop: '2vh'}}>
+        <Typography
+          variant="body1"
+          sx={{margin: 0}}
+        >
+          Country
+        </Typography>
+        <TextField
+          select
+          fullWidth
+          value={country}
+          size="small"
+        >
+          {Object.keys(locations).map((country) => (
+            <MenuItem key={country} value={country} onClick={() => setCountry(country)}>
+              {country}
+            </MenuItem>
+          ))}
+        </TextField>
+      </Box>
+      <Box sx={{marginTop: '2vh'}}>
+        <Typography
+          variant="body1"
+          sx={{margin: 0}}
+        >
+          State
+        </Typography>
+        <TextField
+          select
+          fullWidth
+          value={state}
+          size="small"
+        >
+          {locations[country].map((state) => (
+            <MenuItem key={state} value={state} onClick={() => setState(state)}>
+              {state}
+            </MenuItem>
+          ))}
+        </TextField>
+      </Box>
       <Button
         onClick={submitVehicle}
         sx={{
@@ -123,3 +166,106 @@ export default function View() {
     </Box>
   )
 }
+
+
+const locations = {
+  "United States": [
+    "Alabama",
+    "Alaska",
+    "Arizona",
+    "Arkansas",
+    "California",
+    "Colorado",
+    "Connecticut",
+    "Delaware",
+    "Florida",
+    "Georgia",
+    "Hawaii",
+    "Idaho",
+    "Illinois",
+    "Indiana",
+    "Iowa",
+    "Kansas",
+    "Kentucky",
+    "Louisiana",
+    "Maine",
+    "Maryland",
+    "Massachusetts",
+    "Michigan",
+    "Minnesota",
+    "Mississippi",
+    "Missouri",
+    "Montana",
+    "Nebraska",
+    "Nevada",
+    "New Hampshire",
+    "New Jersey",
+    "New Mexico",
+    "New York",
+    "North Carolina",
+    "North Dakota",
+    "Ohio",
+    "Oklahoma",
+    "Oregon",
+    "Pennsylvania",
+    "Rhode Island",
+    "South Carolina",
+    "South Dakota",
+    "Tennessee",
+    "Texas",
+    "Utah",
+    "Vermont",
+    "Virginia",
+    "Washington",
+    "West Virginia",
+    "Wisconsin",
+    "Wyoming"
+  ],
+  "Canada": [
+    "Alberta",
+    "Manitoba",
+    "New Brunswick",
+    "Newfoundland and Labrador",
+    "Northwest Territories",
+    "Nova Scotia",
+    "Nunavut",
+    "Ontario",
+    "Prince Edward Island",
+    "Quebec",
+    "Saskatchewan",
+    "Yukon"
+  ],
+  "Mexico": [
+    "Aguascalientes",
+    "Baja California",
+    "Baja California Sur",
+    "Campeche",
+    "Chiapas",
+    "Chihuahua",
+    "Coahuila",
+    "Colima",
+    "Durango",
+    "Guanajuato",
+    "Guerrero",
+    "Hidalgo",
+    "Jalisco",
+    "Mexico",
+    "Michoacan",
+    "Morelos",
+    "Nayarit",
+    "Nuevo Leon",
+    "Oaxaca",
+    "Puebla",
+    "Queretaro",
+    "Quintana Roo",
+    "San Luis Potosi",
+    "Sinaloa",
+    "Sonora",
+    "Tabasco",
+    "Tamaulipas",
+    "Tlaxcala",
+    "Veracruz",
+    "Yucatan",
+    "Zacatecas"
+  ]
+} as Record<string, string[]>

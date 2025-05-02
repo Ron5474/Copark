032b6df7282af5d05f70e3f009b8c1c7fd5164d9
Ronak Patel <roapatel@ucsc.edu>
2025-05-01 22:28:24 -0700

    partial login UI

---
+++ app/driver/src/app/login/page.tsx
@@ -0,0 +1,22 @@
+/*
+#######################################################################
+#
+# Copyright (C) 2025 Copark Inc. All right reserved.
+#
+# You may not use, distribute, publish, or modify this code without 
+# the express written permission of the copyright holder.
+#
+#######################################################################
+*/
+
+import type { NextPage } from 'next'
+
+import LoginView from './View'
+
+const Page: NextPage = () => {
+  return (
+    <LoginView />
+  )
+}
+
+export default Page

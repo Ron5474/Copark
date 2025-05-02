dc027f46cfd5cff581383fdb3f46f1453f335d62
Swayam Shah <sshah36@ucsc.edu>
2025-05-02 14:47:28 -0700

    Change image create footer

---
+++ app/driver/src/app/components/footer.tsx
@@ -0,0 +1,15 @@
+/**
+ * @file footer.tsx
+ * @description This file contains the Footer component.
+ * @author Swayam Shah
+ */
+
+
+function Footer() {
+  return (
+    <footer style={{backgroundColor: 'theme.palette.primary.light'}}>
+    </footer>
+  )
+  }
+
+export default Footer;

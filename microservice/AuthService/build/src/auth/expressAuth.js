efc2833da2061f2056a8c32ba55291b5bbdfd08c
Ronak Patel <roapatel@ucsc.edu>
2025-05-02 17:12:10 -0700

    deleted build files

--- microservice/AuthService/build/src/auth/expressAuth.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.expressAuthentication = expressAuthentication;
-const service_1 = require("./service");
-function expressAuthentication(request, securityName, scopes) {
-    return new service_1.AuthService().check(request.headers.authorization, scopes);
-}

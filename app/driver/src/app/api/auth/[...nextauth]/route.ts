8ea8c1eebb00636896013077e461b5dd4bc6db82
Swayam Shah <sshah36@ucsc.edu>
2025-05-03 19:57:11 -0700

    Add environment variables to CI pipeline

--- app/driver/src/app/api/auth/[...nextauth]/route.ts
+++ app/driver/src/app/api/auth/[...nextauth]/route.ts
@@ -4,20 +4,16 @@ import NextAuth from "next-auth"
 // import GitLab from "next-auth/providers/gitlab"
 import Google from "next-auth/providers/google"
 
-// if (!process.env.GOOGLE_ID || !process.env.GOOGLE_SECRET) {
-//   throw new Error("Missing Google OAuth credentials in environment variables")
-// }
+if (!process.env.GOOGLE_ID || !process.env.GOOGLE_SECRET) {
+  throw new Error("Missing Google OAuth credentials in environment variables")
+}
 
 export const authOptions = {
   providers: [
-//    // Google({
-//     //   clientId: process.env.GOOGLE_ID,
-//     //   clientSecret: process.env.GOOGLE_SECRET,
-//     // }),
-    Google({
-      clientId: '512602013845-tir6qaf1v2cf1q51a7prq26cbf6ge37p.apps.googleusercontent.com',
-      clientSecret: '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',
-    }),
+   Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    })
   ],
 }
 

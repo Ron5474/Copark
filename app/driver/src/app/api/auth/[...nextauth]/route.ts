e3583abef4411907815b4ed81058a0e7e3fa4e85
Swayam Shah <sshah36@ucsc.edu>
2025-05-03 20:40:01 -0700

    FIx Oauth

--- app/driver/src/app/api/auth/[...nextauth]/route.ts
+++ app/driver/src/app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,21 @@
 import NextAuth from "next-auth"
+import dotenv from "dotenv"
 
 // import Facebook from "next-auth/providers/facebook"
 // import GitLab from "next-auth/providers/gitlab"
 import Google from "next-auth/providers/google"
 
-if (!process.env.GOOGLE_ID || !process.env.GOOGLE_SECRET) {
+dotenv.config({path: '@/driver/.env'})
+
+if (!process.env.NEXT_PUBLIC_GOOGLE_ID || !process.env.NEXT_PUBLIC_GOOGLE_SECRET) {
   throw new Error("Missing Google OAuth credentials in environment variables")
 }
 
 export const authOptions = {
   providers: [
    Google({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET,
     })
   ],
 }

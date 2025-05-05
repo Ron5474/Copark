00245c0b1ea634729f4acfd55b19a94159b67283
Ronak Patel <roapatel@ucsc.edu>
2025-05-05 13:59:25 -0700

    build success

--- app/driver/src/app/[locale]/login/actions.ts
+++ app/driver/src/app/[locale]/login/actions.ts
@@ -1,10 +1,10 @@
-"use server"
+// "use server"
 
-import { signIn } from "@/auth"
+// import { signIn } from "@/auth"
 
-export async function login() {
-  await signIn('google', {redirectTo: '/'})
+// export async function login() {
+//   await signIn('google', {redirectTo: '/'})
   
-  // Do something with the session data
-  // return session?.user || null
-}
+//   // Do something with the session data
+//   // return session?.user || null
+// }

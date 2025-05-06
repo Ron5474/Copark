e8dd2ec3dff369f9dac6755d5e0f45ac39ba2324
Bryant Oliver <bcoliver@ucsc.edu>
2025-05-06 15:49:44 -0700

    Fixed AddForm failed test

--- app/driver/src/app/[locale]/selectVehicle/AddForm.tsx
+++ app/driver/src/app/[locale]/selectVehicle/AddForm.tsx
@@ -178,7 +178,7 @@ export default function AddForm({ isGuest = false }: { isGuest?: boolean }) {
           <TextField
             required
             fullWidth
-            value={nickname}
+            value={nickname || ''}
             slotProps={{
               input: {
                 inputProps: {

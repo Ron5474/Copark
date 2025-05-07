8131ea23e8fd90230ad08ad57faa45ad63d3db1b
Swayam Shah <sshah36@ucsc.edu>
2025-05-06 22:57:52 -0700

    FIX env vars for db in microservices

--- microservice/AuthService/src/auth/db.ts
+++ microservice/AuthService/src/auth/db.ts
@@ -12,7 +12,7 @@ dotenv.config({path: '../../.env'})
 const pool = new Pool({
   host: (process.env.POSTGRES_HOST || 'localhost'),
   port: process.env.POSTGRES_PORT ? parseInt(process.env.POSTGRES_PORT, 10) : 5432,
-  database: process.env.POSTGRES_DB,
+  database: process.env.POSTGRES_AUTH_DB,
   // database: 'auth',
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,

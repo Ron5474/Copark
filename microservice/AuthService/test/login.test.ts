563d291c45f9814632d1e52c5ef17e62a5dfddd3
Ronak Patel <roapatel@ucsc.edu>
2025-05-08 21:34:53 -0700

    OAuth related microservice pass

--- microservice/AuthService/test/login.test.ts
+++ microservice/AuthService/test/login.test.ts
@@ -28,16 +28,89 @@ afterAll(() => {
   db.shutdown()
 })
 
-test('Correct User Login', async () => {
-  const user = await new AuthService().driverLogin({
+test('OAuth User can login successfully', async () => {
+  const driver = {
     "name": "Derik Driver",
     "email": "derik@copark.space",
     "picture": "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExYWRzMmJldTdzMWtncDBweGtvM21kYnRyeDk1cHpvNnU5MWVycXEybiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/keyufLabLaJKh3xnVy/giphy.gif",
     "sub": "1234567890",
-  })
+  }
+  const user = await new AuthService().driverLogin(driver)
   expect(user).toBeDefined()
 })
 
+test('OAuth User login twice returns undefined', async () => {
+  const driver = {
+    "name": "Derik Driver",
+    "email": "derik@copark.space",
+    "picture": "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExYWRzMmJldTdzMWtncDBweGtvM21kYnRyeDk1cHpvNnU5MWVycXEybiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/keyufLabLaJKh3xnVy/giphy.gif",
+    "sub": "1234567890",
+  }
+  await new AuthService().driverLogin(driver)
+  const user = await new AuthService().driverLogin(driver)
+  expect(user).not.toBeDefined()
+})
+
+test('OAuth Undefined user throws error', async () => {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  vi.spyOn(console, 'error').mockImplementation(() => {})
+  await expect(new AuthService().driverLogin(undefined)).rejects.toThrow('Unauthorized')
+})
+
+test('OAuth user with id throws error', async () => {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  vi.spyOn(console, 'error').mockImplementation(() => {})
+  const driver = {
+    "name": "Derik Driver",
+    "email": "derik@copark.space",
+    "picture": "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExYWRzMmJldTdzMWtncDBweGtvM21kYnRyeDk1cHpvNnU5MWVycXEybiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/keyufLabLaJKh3xnVy/giphy.gif",
+    "sub": "1234567890",
+    "id": "fake-id",
+  }
+  await expect(new AuthService().driverLogin(driver)).rejects.toThrow('Unauthorized')
+})
+
+test('getOauthUser() returns JWT for the current user', async () => {
+  const driver = {
+    "name": "Derik Driver",
+    "email": "derik@copark.space",
+    "picture": "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExYWRzMmJldTdzMWtncDBweGtvM21kYnRyeDk1cHpvNnU5MWVycXEybiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/keyufLabLaJKh3xnVy/giphy.gif",
+    "sub": "1234567890",
+  }
+  await new AuthService().driverLogin(driver)
+  const user = await new AuthService().getOauthUser(driver)
+  expect(user).toMatch(/^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/) // JWT Regex Comparison
+})
+
+test('getOauthUser() Undefined user throws error', async () => {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  vi.spyOn(console, 'error').mockImplementation(() => {})
+  await expect(new AuthService().getOauthUser(undefined)).rejects.toThrow('Unauthorized')
+})
+
+test('getOauthUser() user with id throws error', async () => {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  vi.spyOn(console, 'error').mockImplementation(() => {})
+  const driver = {
+    "name": "Derik Driver",
+    "email": "derik@copark.space",
+    "picture": "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExYWRzMmJldTdzMWtncDBweGtvM21kYnRyeDk1cHpvNnU5MWVycXEybiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/keyufLabLaJKh3xnVy/giphy.gif",
+    "sub": "1234567890",
+    "id": "fake-id",
+  }
+  await expect(new AuthService().getOauthUser(driver)).rejects.toThrow('Unauthorized')
+})
+
+test('getOauthUser() fake user returns undefined', async () => {
+  const driver = {
+    "name": "Derik Driver",
+    "email": "derik@copark.space",
+    "picture": "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExYWRzMmJldTdzMWtncDBweGtvM21kYnRyeDk1cHpvNnU5MWVycXEybiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/keyufLabLaJKh3xnVy/giphy.gif",
+    "sub": "1234567890",
+  }
+  const user = await new AuthService().getOauthUser(driver)
+  expect(user).not.toBeDefined()
+})
 
 // test('Incorrect User Login', async () => {
 //   const user = await authenticate({email: 'anna@books.com', password: 'fakepass'})

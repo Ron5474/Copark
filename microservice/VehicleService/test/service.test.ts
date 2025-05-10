d98d22f3a8c612f3a80d8aae08f11bd91933319a
Jiancheng Xiong <jxiong32@ucsc.edu>
2025-05-10 00:06:38 -0700

    100% on vehicle services, also added encryption protection

--- microservice/VehicleService/test/service.test.ts
+++ microservice/VehicleService/test/service.test.ts
@@ -13,9 +13,11 @@ import {test, afterAll, expect, vi, beforeEach} from 'vitest'
 
 import db from './db'
 import { VehicleService } from '../src/vehicle/service'
+import { SignJWT, jwtVerify } from 'jose'
 
 vi.mock('server-only', () => ({}))
 
+const encodedKey = new TextEncoder().encode(process.env.MASTER_SECRET + 'apiexit')
 
 beforeEach( async () => {
     return db.reset()
@@ -25,8 +27,33 @@ afterAll(() => {
   db.shutdown()
 })
 
-const mock_driver1_ID = "b20ec061-2957-4c3b-b193-c8b40138e8f1"
-const mock_driver2_ID = "6c83442f-90ca-46ae-bf31-a79ed4a0d1ce"
+async function encrypt(userId: string): Promise<string> {
+  return new SignJWT({ id: userId })
+    .setProtectedHeader({ alg: 'HS256' })
+    .setIssuedAt()
+    .setExpirationTime('30m')
+    .sign(encodedKey)
+  }
+
+// async function decrypt(token: string): Promise<string | undefined> {
+//   try {
+//     const { payload } = await jwtVerify(token, encodedKey)
+
+//     return payload.id as string; // Extract the `id` from the payload
+//   } catch (error) {
+//     void error;
+//     // console.error('Failed to decrypt token:', error);
+//     return undefined; // Return undefined if the token is invalid or expired
+//   }
+// }
+
+let mock_driver1_ID: string;
+let mock_driver2_ID: string;
+
+beforeEach(async () => {
+  mock_driver1_ID = await encrypt("b20ec061-2957-4c3b-b193-c8b40138e8f1");
+  mock_driver2_ID = await encrypt("6c83442f-90ca-46ae-bf31-a79ed4a0d1ce");
+});
 
 const mock_vehicle1 = {
   "plate": "YM2BCC5",

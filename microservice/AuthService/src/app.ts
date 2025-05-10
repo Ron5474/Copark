4fe2b9fc38c14978c9dfbc9f09f315ec1bc9e6e9
Yaquob Mohamed <ysmohame@ucsc.edu>
2025-05-10 12:48:49 -0700

    Fixed Authservice to prev implementation

--- microservice/AuthService/src/app.ts
+++ microservice/AuthService/src/app.ts
@@ -1,45 +1,43 @@
-import 'reflect-metadata'
-import express, {
-  Express,
+import express, { 
+  Express, 
   Router,
-  Response as ExResponse,
-  Request as ExRequest,
+  Response as ExResponse, 
+  Request as ExRequest, 
   ErrorRequestHandler,
   NextFunction
 } from 'express'
 import cors from 'cors'
 import swaggerUi from 'swagger-ui-express'
 
-function createAuthApp(): Express {
-  const app: Express = express()
-  app.use(cors())
-  app.use(express.json())
-  app.use(express.urlencoded({ extended: false }))
+import { RegisterRoutes } from "../build/routes"
 
-  app.use('/api/v0/docs', swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
-    res.send(
-      swaggerUi.generateHTML(await import('../build/swagger.json'))
-    )
+const app: Express = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/api/v0/docs', swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
+  res.send(
+    swaggerUi.generateHTML(await import('../build/swagger.json'))
+  )
+})
+
+const router = Router()
+RegisterRoutes(router)
+app.use('/api/v0', router)
+
+// Enhanced error handler
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const errorHandler: ErrorRequestHandler = (err, _req, res, _next: NextFunction) => {
+  // console.log(process.env.POSTGRES_PASSWORD)
+  // console.error('Error occurred:', err) // Log the error
+
+  res.status(err.status || 500).json({
+    message: err.message || 'Internal Server Error',
+    errors: err.errors || null,
+    status: err.status || 500,
   })
-
-  const { RegisterRoutes } = require('../build/build/routes') // why is this double nested btw
-  const router = Router()
-  RegisterRoutes(router)
-  app.use('/api/v0', router)
-
-  const errorHandler: ErrorRequestHandler = (err, _req, res, _next: NextFunction) => {
-    res.status(err.status || 500).json({
-      message: err.message || 'Internal Server Error',
-      errors: err.errors || null,
-      status: err.status || 500,
-    })
-  }
-  app.use(errorHandler)
-
-  return app
 }
+app.use(errorHandler)
 
-const app = createAuthApp()
-
-export { createAuthApp }
 export default app

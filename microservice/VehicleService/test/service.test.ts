781bd91846b3cec48379231bdefd197a89b26676
Ronak Patel <roapatel@ucsc.edu>
2025-05-09 22:43:36 -0700

    getVehicles Service test

---
+++ microservice/VehicleService/test/service.test.ts
@@ -0,0 +1,64 @@
+/*
+#######################################################################
+#
+# Copyright (C) 2025 Copark Inc. All right reserved.
+#
+# You may not use, distribute, publish, or modify this code without
+# the express written permission of the copyright holder.
+#
+#######################################################################
+*/
+
+import {test, afterAll, expect, vi, beforeEach} from 'vitest'
+
+import db from './db'
+import { VehicleService } from '../src/vehicle/service'
+
+vi.mock('server-only', () => ({}))
+
+
+beforeEach( async () => {
+    return db.reset()
+})
+
+afterAll(() => {
+  db.shutdown()
+})
+
+const mock_driver1_ID = "b20ec061-2957-4c3b-b193-c8b40138e8f1"
+const mock_driver2_ID = "6c83442f-90ca-46ae-bf31-a79ed4a0d1ce"
+
+const mock_vehicle1 = {
+  "plate": "YM2BCC5",
+  "country": "USA",
+  "state": "California"
+}
+
+const mock_vehicle2 = {
+  "plate": "YM2BCC6",
+  "country": "USA",
+  "state": "California",
+  "nickname": "electric fiat"
+}
+
+test('getMyVehicles - Returns Correct Number (0)', async () => {
+
+  const vehicles = await new VehicleService().getMyVehicles(mock_driver1_ID)
+  expect(vehicles.length).toBe(0)
+})
+
+test('getMyVehicles - Returns Correct Number (2)', async () => {
+  await new VehicleService().registerVehicle(mock_driver1_ID, mock_vehicle1)
+  await new VehicleService().registerVehicle(mock_driver1_ID, mock_vehicle2)
+  const vehicles = await new VehicleService().getMyVehicles(mock_driver1_ID)
+  expect(vehicles.length).toBe(2)
+})
+
+test('getMyVehicles - does not show Driver2 vehicles belonging to Driver1', async () => {
+  await new VehicleService().registerVehicle(mock_driver1_ID, mock_vehicle1)
+  await new VehicleService().registerVehicle(mock_driver1_ID, mock_vehicle2)
+  const vehicles = await new VehicleService().getMyVehicles(mock_driver2_ID)
+  expect(vehicles.length).toBe(0)
+})
+
+

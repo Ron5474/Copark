ff8af339b9b34572321adb9d86ec94f1414b5dda
Atharva Darshan Tawde <atawde@ucsc.edu>
2025-05-12 14:21:23 -0700

    add api user frontend v1

---
+++ app/admin/src/app/components/AddAPIUser.tsx
@@ -0,0 +1,97 @@
+'use client'
+
+import React, { useState } from 'react'
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField,
+  useTheme,
+  Typography
+} from '@mui/material'
+import { addAPIUser } from '../../api/actions'
+
+export default function AddAPIUser({ open, onClose }: { open: boolean, onClose: () => void }) {
+  const theme = useTheme()
+  const [newUser, setNewUser] = useState({
+    name: '',
+    email: '',
+    role: 'api_user' // Default role
+  })
+
+  const handleSubmit = async () => {
+    await addAPIUser(newUser)
+    setNewUser({
+      name: '',
+      email: '',
+      role: 'api_user'
+    })
+    onClose()
+  }
+
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+      PaperProps={{
+        sx: {
+          borderRadius: '15px',
+          p: 2
+        }
+      }}
+    >
+      <DialogTitle sx={{ 
+        color: theme.palette.primary.main,
+        fontWeight: 700 
+      }}>
+        Add API User
+      </DialogTitle>
+      <DialogContent>
+        <Box sx={{ mt: 2 }}>
+          <Typography variant="subtitle2" sx={{ mb: 1 }}>
+            Enter organization details
+          </Typography>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Organization Name"
+            fullWidth
+            value={newUser.name}
+            onChange={(e) => setNewUser({ ...newUser, name: e.target.value })}
+            sx={{ mb: 2 }}
+          />
+          <TextField
+            margin="dense"
+            label="Organization Email"
+            fullWidth
+            value={newUser.email}
+            onChange={(e) => setNewUser({ ...newUser, email: e.target.value })}
+          />
+        </Box>
+      </DialogContent>
+      <DialogActions>
+        <Button 
+          onClick={onClose}
+          sx={{ color: theme.palette.secondary.main }}
+        >
+          Cancel
+        </Button>
+        <Button 
+          onClick={handleSubmit}
+          sx={{
+            bgcolor: theme.palette.primary.main,
+            color: '#ffffff',
+            '&:hover': {
+              bgcolor: theme.palette.primary.dark
+            }
+          }}
+        >
+          Add
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}

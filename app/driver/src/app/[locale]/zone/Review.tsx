1269bd15d5308e86fcac02a761f81d76881028ee
Bryant Oliver <bcoliver@ucsc.edu>
2025-05-15 11:44:05 -0700

    Added stripe reroute button

--- app/driver/src/app/[locale]/zone/Review.tsx
+++ app/driver/src/app/[locale]/zone/Review.tsx
@@ -6,17 +6,11 @@
 
 'use client'
 
-import { Fragment, /*useEffect, useState,*/ useContext } from 'react'
+import { useContext } from 'react'
 import {
   Box,
+  Button,
   Divider,
-  // Button,
-  // Dialog,
-  // DialogContent,
-  // FormControlLabel,
-  // IconButton,
-  // Radio,
-  // RadioGroup,
   Typography,
 } from '@mui/material'
 // import CloseIcon from '@mui/icons-material/Close'
@@ -34,146 +28,164 @@ export default function Review() {
   
   const tempLocation = "UCSC Campus"
 
+  const checkout = () => {
+    console.log("Reroute to stripe") // delete this
+  }
+
   return (
-    <Fragment>
-      <Box sx={{
-          maxWidth: '92%',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-between',
-          margin: 'auto',
-          marginTop: '1vh',
-        }}
+    <Box sx={{
+        maxWidth: '92%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        margin: 'auto',
+        marginTop: '1vh',
+      }}
+    >
+      <Typography
+        variant="h5"
+        gutterBottom
+        fontWeight={600}
       >
+        Review Order
+      </Typography>
+
+      <Divider sx={{ my: 2 }}/>
+
+      <Box >
         <Typography
-          variant="h5"
+          variant="body1"
           gutterBottom
           fontWeight={600}
         >
-          Review Order
+          Zone
         </Typography>
-
-        <Divider sx={{ my: 2 }}/>
-
-        <Box >
-          <Typography
-            variant="body1"
-            gutterBottom
-            fontWeight={600}
-          >
-            Zone
-          </Typography>
-          <Typography
-            variant="body1"
-            gutterBottom
-          >
-            {`Zone # ${zoneNumber}`}
-          </Typography>
-          <Typography
-            variant="body1"
-            gutterBottom
-          >
-            {`Location: ${tempLocation}`}
-          </Typography>
-        </Box>
-
-        <Divider sx={{ my: 2 }}/>
-
-        <Box>
-          <Typography
-            variant="body1"
-            gutterBottom
-            fontWeight={600}
-          >
-            Duration
-          </Typography>
-          <Typography
-            variant="body1"
-            gutterBottom
-          >
-            {`${durationString}`}
-          </Typography>
-        </Box>
-
-        <Divider sx={{ my: 2 }}/>
-
-        <Box>
-          <Typography
-            variant="body1"
-            gutterBottom
-            fontWeight={600}
-          >
-            Vehicle
-          </Typography>
-          <Typography
-            variant="body1"
-            gutterBottom
-          >
-            {`${vehicle?.nickname} - ${vehicle?.plate} (${vehicle?.state})`}
-          </Typography>
-        </Box>
-
-        <Divider sx={{ my: 2 }}/>
-
-        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Typography
-            variant="body1"
-            gutterBottom
-          >
-            Parking Subtotal
-          </Typography>
-          <Typography
-            variant="body1"
-            gutterBottom
-          >
-            {`$${price.toFixed(2)}`}
-          </Typography>
-        </Box>
-
-        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Typography
-            variant="body1"
-            gutterBottom
-          >
-            Transaction Fee
-          </Typography>
-          <Typography
-            variant="body1"
-            gutterBottom
-          >
-            {`$${0.5.toFixed(2)}`}
-          </Typography>
-        </Box>
-
-        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Typography
-            variant="h6"
-            gutterBottom
-            fontWeight={600}
-          >
-            Order Total
-          </Typography>
-          <Typography
-            variant="h6"
-            gutterBottom
-            color="primary"
-            fontWeight={600}
-          >
-            {`$${(price + 0.5).toFixed(2)}`}
-          </Typography>
-        </Box>
-
-        <Box>
-          <Typography
-            variant="subtitle2"
-            gutterBottom
-            color="gray"
-          >
-            Parking taxes may be included in subtotal. This transaction is non-refundable.
-          </Typography>
-        </Box>
-
+        <Typography
+          variant="body1"
+          gutterBottom
+        >
+          {`Zone # ${zoneNumber}`}
+        </Typography>
+        <Typography
+          variant="body1"
+          gutterBottom
+        >
+          {`Location: ${tempLocation}`}
+        </Typography>
       </Box>
-    </Fragment>
+
+      <Divider sx={{ my: 2 }}/>
+
+      <Box>
+        <Typography
+          variant="body1"
+          gutterBottom
+          fontWeight={600}
+        >
+          Duration
+        </Typography>
+        <Typography
+          variant="body1"
+          gutterBottom
+        >
+          {`${durationString}`}
+        </Typography>
+      </Box>
+
+      <Divider sx={{ my: 2 }}/>
+
+      <Box>
+        <Typography
+          variant="body1"
+          gutterBottom
+          fontWeight={600}
+        >
+          Vehicle
+        </Typography>
+        <Typography
+          variant="body1"
+          gutterBottom
+        >
+          {`${vehicle?.nickname} - ${vehicle?.plate} (${vehicle?.state})`}
+        </Typography>
+      </Box>
+
+      <Divider sx={{ my: 2 }}/>
+
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Typography
+          variant="body1"
+          gutterBottom
+        >
+          Parking Subtotal
+        </Typography>
+        <Typography
+          variant="body1"
+          gutterBottom
+        >
+          {`$${price.toFixed(2)}`}
+        </Typography>
+      </Box>
+
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Typography
+          variant="body1"
+          gutterBottom
+        >
+          Transaction Fee
+        </Typography>
+        <Typography
+          variant="body1"
+          gutterBottom
+        >
+          {`$${0.5.toFixed(2)}`}
+        </Typography>
+      </Box>
+
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Typography
+          variant="h6"
+          gutterBottom
+          fontWeight={600}
+        >
+          Order Total
+        </Typography>
+        <Typography
+          variant="h6"
+          gutterBottom
+          color="primary"
+          fontWeight={600}
+        >
+          {`$${(price + 0.5).toFixed(2)}`}
+        </Typography>
+      </Box>
+
+      <Box>
+        <Typography
+          variant="subtitle2"
+          gutterBottom
+          color="gray"
+        >
+          Parking taxes may be included in subtotal. This transaction is non-refundable.
+        </Typography>
+      </Box>
+
+      <Button
+        fullWidth
+        variant="contained"
+        onClick={checkout}
+        sx={{
+          width: '100%',
+          marginTop: '5vh',
+          fontSize: '1.15rem',
+          color: 'white',
+          backgroundColor: "primary",
+          textTransform: 'none',
+        }}
+      >
+        Continue to payment
+      </Button>
+
+    </Box>
   )
 }

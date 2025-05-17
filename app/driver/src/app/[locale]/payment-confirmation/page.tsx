5068ac47796a0fcd30a52d3e5f4ed1479064fab9
Swayam Shah <sshah36@ucsc.edu>
2025-05-16 22:23:17 -0700

    Success page svg added

--- app/driver/src/app/[locale]/payment-confirmation/page.tsx
+++ app/driver/src/app/[locale]/payment-confirmation/page.tsx
@@ -4,7 +4,7 @@ import { useSearchParams } from "next/navigation";
 import Topbar from "../shared/Topbar";
 import { useEffect } from "react";
 import { getTransactionDetails } from "./actions";
-import { Box } from "@mui/material";
+import { Box, Toolbar } from "@mui/material";
 
 function PaymentConfirmation() {
   const sessionId = useSearchParams().get("session_id");
@@ -22,10 +22,13 @@ function PaymentConfirmation() {
   return (
     <>
       <Topbar />
+      <Toolbar />
+      <Toolbar />
+      <Toolbar />
       <Box sx={{ padding: "20px", display: "flex", flexDirection: "column", alignItems: "center" }}>
       <picture>
         <source srcSet="/driver/assets/tick.svg" type="image/svg+xml" />
-        <img src="/driver/assets/tick.svg" alt="payment confirmed" width={100} height={100} aria-label="tick" />
+        <img src="/driver/assets/tick.svg" alt="payment confirmed" width={300} height={300} aria-label="tick" />
       </picture>
 
       </Box>

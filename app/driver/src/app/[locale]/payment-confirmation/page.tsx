6009f9c11a3da176ec24a6800205d89927fff470
Swayam Shah <sshah36@ucsc.edu>
2025-05-16 22:10:52 -0700

    added the tick.svg

--- app/driver/src/app/[locale]/payment-confirmation/page.tsx
+++ app/driver/src/app/[locale]/payment-confirmation/page.tsx
@@ -4,6 +4,7 @@ import { useSearchParams } from "next/navigation";
 import Topbar from "../shared/Topbar";
 import { useEffect } from "react";
 import { getTransactionDetails } from "./actions";
+import { Box } from "@mui/material";
 
 function PaymentConfirmation() {
   const sessionId = useSearchParams().get("session_id");
@@ -21,6 +22,13 @@ function PaymentConfirmation() {
   return (
     <>
       <Topbar />
+      <Box sx={{ padding: "20px", display: "flex", flexDirection: "column", alignItems: "center" }}>
+      <picture>
+        <source srcSet="/driver/assets/tick.svg" type="image/svg+xml" />
+        <img src="/driver/assets/tick.svg" alt="payment confirmed" width={100} height={100} aria-label="tick" />
+      </picture>
+
+      </Box>
     </>
   );
 }

f7865bb9a6a80bb8807d1f3023eb6b7148bdf7af
Yaquob Mohamed <ysmohame@ucsc.edu>
2025-05-18 20:04:03 -0700

    removed consloe logs

--- microservice/EmailService/src/email/mailer.ts
+++ microservice/EmailService/src/email/mailer.ts
@@ -4,9 +4,6 @@ import dotenv from 'dotenv'
 import { resolve } from 'path';
 
 dotenv.config({ path: resolve(__dirname, '../../../../.env') })
-console.log("EMAIL_HOST:", process.env.EMAIL_HOST)
-console.log("SMTP_PORT:", process.env.SMTP_PORT)
-console.log("EMAIL_USER:", process.env.EMAIL_USER)
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
@@ -18,10 +15,6 @@ const transporter = nodemailer.createTransport({
 })
 
 export const sendEmail = async ({ to, subject, text, html }: EmailRequest) => {
-  console.log("EMAIL_HOST:", process.env.EMAIL_HOST)
-  console.log("SMTP_PORT:", process.env.SMTP_PORT)
-  console.log("EMAIL_USER:", process.env.EMAIL_USER)
-  console.log("EMAIL_PASS is set:", !!process.env.EMAIL_PASS)
   await transporter.sendMail({
     from: `"CoPark" <${process.env.EMAIL_USER}>`,
     to,

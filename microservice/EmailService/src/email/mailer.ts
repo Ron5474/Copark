74c5c77ad7c91cbc13cbc95eb55cdcf5c56b4a3c
Yaquob Mohamed <ysmohame@ucsc.edu>
2025-05-20 10:44:21 -0700

    added email service to permit service

--- microservice/EmailService/src/email/mailer.ts
+++ microservice/EmailService/src/email/mailer.ts
@@ -8,11 +8,17 @@ dotenv.config({ path: resolve(__dirname, '../../../../.env') })
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.SMTP_PORT),
+  secure: Number(process.env.SMTP_PORT) === 465,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-})
+  tls: {
+    rejectUnauthorized: true,
+    minVersion: 'TLSv1.2'
+  }
+});
 
 export const sendEmail = async ({ to, subject, text, html }: EmailRequest) => {
   await transporter.sendMail({

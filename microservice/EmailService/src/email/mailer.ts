fc618a8041add3affaffa554f2dd49644617d63a
Swayam Shah <sshah36@ucsc.edu>
2025-05-20 11:47:44 -0700

    Fix env file issue in email service

--- microservice/EmailService/src/email/mailer.ts
+++ microservice/EmailService/src/email/mailer.ts
@@ -12,10 +12,16 @@ dotenv.config({ path: p})
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.SMTP_PORT),
+  secure: Number(process.env.SMTP_PORT) === 465,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: true,
+    minVersion: 'TLSv1.2'
+  }
 })
 
 export const sendEmail = async ({ to, subject, text, html }: EmailRequest) => {

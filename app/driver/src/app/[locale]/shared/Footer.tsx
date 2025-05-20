4ae80bb20c692101e0bf5d07f54407b09cfdb850
Ronak Patel <roapatel@ucsc.edu>
2025-05-19 21:50:24 -0700

    removed Dark Mode button and Do not sell my Info from Footer because it is not implemented

--- app/driver/src/app/[locale]/shared/Footer.tsx
+++ app/driver/src/app/[locale]/shared/Footer.tsx
@@ -3,7 +3,7 @@
  * @description This file contains the Footer component.
  * @author Swayam Shah
  */
-import { Box, Button } from "@mui/material";
+import { Box } from "@mui/material";
 // import { getTranslations } from "next-intl/server";
 import { useTranslations } from "next-intl";
 import theme from "../theme";
@@ -53,11 +53,11 @@ function Footer() {
           color: "#000000",
           fontWeight: 500,
         }}>
-          <a aria-label="personal-info-link">{t("Do Not Sell My Personal Info")}</a>
+          {/* <a aria-label="personal-info-link">{t("Do Not Sell My Personal Info")}</a> */}
           <a onClick={() => router.push('/privacy')} aria-label="privacy-policy-link">{t("Privacy Policy")}</a>
           <a onClick={() => router.push('/tos')} aria-label="service-terms-link">{t("Terms of Service")}</a>
           <a aria-label="contact-us-link">{t("Contact Us")}</a>
-          <Button sx={{padding: 0}}>{t("Dark Mode")}</Button> {/* TODO: Change theme and change this text based on current theme */}
+          {/* <Button sx={{padding: 0}}>{t("Dark Mode")}</Button> TODO: Change theme and change this text based on current theme */}
         </Box>
       </Box>
       <p style={{ fontSize: "14px", color: "#000000", fontWeight: 500 }}>{t("Rights Reserved")}</p>

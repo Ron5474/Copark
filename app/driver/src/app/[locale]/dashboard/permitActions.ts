c90c360c0dfcb56fde700381c39095fe9232deee
Yaquob Mohamed <ysmohame@ucsc.edu>
2025-05-22 21:25:16 -0700

    added actions for permit displaying

---
+++ app/driver/src/app/[locale]/dashboard/permitActions.ts
@@ -0,0 +1,39 @@
+'use server';
+
+import { cookies } from "next/headers";
+import { LotGroup } from "../types";
+
+export const getLotDetails = async (): Promise<LotGroup[]> => {
+  const token = (await cookies()).get(process.env.NEXT_PUBLIC_AUTH_COOKIE_NAME!)?.value
+
+  const response = await fetch('http://localhost:4003/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      query: `
+        query GetLotDetails {
+          allLotDetails {
+            id
+            title
+            lots {
+              name
+              price
+            }
+          }
+        }
+      `,
+    }),
+  })
+
+  const result = await response.json()
+
+  if (result.errors) {
+    console.error(result.errors)
+    throw new Error('Failed to fetch lot details')
+  }
+
+  return result.data.allLotDetails
+}

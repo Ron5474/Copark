72024f2738058e0978534a6b23cf8d544a6c5fea
Ronak Patel <roapatel@ucsc.edu>
2025-05-23 22:52:40 -0700

    Added ChallengeTicket UI

---
+++ app/driver/src/app/[locale]/ticket/ChallengeTicket.tsx
@@ -0,0 +1,14 @@
+import Typography from '@mui/material/Typography'
+// import Chip from '@mui/material/Chip'
+// import Box from '@mui/material/Box'
+
+// import { useTicketState } from './TicketContext'
+// import { Ticket } from '../types'
+
+export default function ChallengeTicket() {
+  return (
+    <Typography>
+        This is the Challenge Ticket Component.
+    </Typography>
+  );
+}

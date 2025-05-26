2784c392125025d7163beb3ac6f3e6da1cd909a0
Bryant Oliver <bcoliver@ucsc.edu>
2025-05-26 16:35:42 -0700

    Fixed driver signup in permit resolver test. Some tests still broke due to police JWT

--- microservice/PermitService/test/resolver.test.ts
+++ microservice/PermitService/test/resolver.test.ts
@@ -2,6 +2,7 @@ import { test, beforeAll, afterAll, expect, beforeEach } from 'vitest'
 // @ts-expect-error: supertest types may not match expected types in this context
 import supertest from 'supertest'
 import * as http from 'http'
+import { SignJWT } from 'jose'
 
 import db from './db'
 import { app, bootstrap } from '../src/app'
@@ -18,8 +19,8 @@ const AUTH_PORT = 3010
 const AUTH_SERVICE_URL = `http://localhost:${AUTH_PORT}`
 const VEHC_PORT = 4001
 // const VEHC_SERVICE_URL = `http://localhost:${VEHC_PORT}`
+const encodedKey = new TextEncoder().encode(process.env.JWT_SECRET)
 
-// const encodedKey = new TextEncoder().encode(process.env.MICROSERVICE_INTERNAL_SECRET)
 
 beforeAll(async () => {
   // Start your GraphQL server
@@ -52,7 +53,13 @@ afterAll(async () => {
   if (vehcServer) vehcServer.close()
 })
 
-const nextAuthJWT = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE3NDY3Njg5MjMsImV4cCI6MTg0MTQ2MzQ0MiwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoiMTA5MTY0MjQwOTk2MDEyNTE1NiIsImVtYWlsIjoiZGVyaWtAY29wYXJrLnNwYWNlIiwicGljdHVyZSI6IlwiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUNnOG9jS2JTT2M0MFc3ZEpJd1VkanNZQzNVSmdwUzdRSjBSR2Yyb3ZKSXF6S3ZzbW1NUFBnPXM5Ni1jIiwibmFtZSI6IkRlcmlrIERyaXZlciJ9.D23uY9TRN-3UKSK8NxdgSP208iaCc8TuzWIYgYMfhwE"
+const driver = {
+  "name": "Derik Driver",
+  "email": "derik@copark.space",
+  "picture": "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExYWRzMmJldTdzMWtncDBweGtvM21kYnRyeDk1cHpvNnU5MWVycXEybiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/keyufLabLaJKh3xnVy/giphy.gif",
+  "sub": "1234567890",
+}
+
 
 // TODO update JWT with new secret
 const policeJWT = "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6ImFiZTQwNWM2LTc0MDAtNGQyMy05Zjg2LTAwZWFkMTU3MjlmNSIsImlhdCI6MTc0NzI2MzIyMSwiZXhwIjoxOTA1MDUxMjIxfQ.UVVWAjg2-asw8gfqoxljHZSVX4Mn_1FzOV85CagVbUQ"
@@ -62,20 +69,56 @@ const adminUser = {
   password: 'password1',
 }
 
-async function loginAs(who: string): Promise<string | undefined> {
+async function loginAs(who: string, defaultVehicle=true): Promise<string | undefined> {
   if (who === "driver") {
-    const response = await supertest(AUTH_SERVICE_URL)
-      .post('/api/v0/auth/driver/login')
-      .set('Authorization', `Bearer ${nextAuthJWT}`)
+    const token = await  new SignJWT(driver)
+      .setProtectedHeader({ alg: 'HS256' })
+      .setIssuedAt()
+      .setExpirationTime('30m')
+      .sign(encodedKey)
 
-    if (response.status !== 200) {
+    // Sign up
+    await supertest(AUTH_SERVICE_URL)
+      .post('/api/v0/auth/driver/signup')
+      .send({ authToken: token })
+
+    // Add Vehicle
+    const addVehicle = await supertest(vehcServer)
+      .post('/graphql')
+      .set('Authorization', 'Bearer ' + token)
+      .send({ 
+        query: regVehicleQuery,
+        variables: derikVehicleInput
+      })
+
+    const id = addVehicle.body.data.registerVehicle.id
+    const setDefaultVehicleInput = {
+      input: {
+        id: id
+      }
+    }
+    if (defaultVehicle) {
+      await supertest(server)
+        .post('/graphql')
+        .set('Authorization', 'Bearer ' + token)
+        .send({ 
+          query: SetDefaultVehicle,
+          variables: setDefaultVehicleInput
+        })
+    }
+    
+    const response = await supertest(AUTH_SERVICE_URL)
+      .put('/api/v0/auth/driver/onboarding')
+      .set('Authorization', `Bearer ${token}`)
+      .send({newState: 'complete'})
+
+    const validStatuses = [200, 201, 204];
+    if (!validStatuses.includes(response.status)) {
       throw new Error(`Login failed with status ${response.status}`)
     }
 
-    return response.body
-  }
-
-  else if (who === "enforcement") {
+    return token
+  } else if (who === "enforcement") {
     const response = await supertest(AUTH_SERVICE_URL)
       .post('/api/v0/auth/login')
       .send({
@@ -86,12 +129,8 @@ async function loginAs(who: string): Promise<string | undefined> {
     if (response.status !== 200) {
       throw new Error(`Enforcement login failed with status ${response.status}`)
     }
-
     return response.body.id
-
-  } 
-  
-  else {
+  } else {
     const response = await supertest(AUTH_SERVICE_URL)
       .post('/api/v0/auth/login')
       .send(adminUser)
@@ -99,8 +138,34 @@ async function loginAs(who: string): Promise<string | undefined> {
     if (response.status !== 200) {
       throw new Error(`Login failed with status ${response.status}`)
     }
-
     return response.body?.id
+  }
+}
+
+const regVehicleQuery = `
+mutation RegisterVehicle($input: RegisterVehicleInput!) {
+  registerVehicle(input: $input) {
+    id
+    plate
+    country
+    state
+    nickname
+  }
+}`
+
+const SetDefaultVehicle = `
+mutation SetDefaultVehicle($input: setDefaultVehicleInput!) {
+  setDefaultVehicle(input: $input) {
+    id
+  }
+}`
+
+const derikVehicleInput = {
+  input: {
+    plate: "DERIK123",
+    country: "US",
+    state: "California",
+    nickname: "Derik's Vehicle"
   }
 }
 

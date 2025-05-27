07d96aac53d196a62f63dc1ee9f547dbbeb4d596
Swayam Shah <sshah36@ucsc.edu>
2025-05-26 22:37:41 -0700

    Bash script to copy all sql

---
+++ copySchema.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+scp -r -i ../coparkxyz.pem ./microservice/AdminService/sql/ ubuntu@copark.space:~/copark/microservice/AdminService/sql
+scp -r -i ../coparkxyz.pem ./microservice/AuthService/sql/ ubuntu@copark.space:~/copark/microservice/AuthService/sql
+scp -r -i ../coparkxyz.pem ./microservice/PaymentService/sql/ ubuntu@copark.space:~/copark/microservice/PaymentService/sql
+scp -r -i ../coparkxyz.pem ./microservice/PermitService/sql/ ubuntu@copark.space:~/copark/microservice/PermitService/sql
+scp -r -i ../coparkxyz.pem ./microservice/TicketService/sql/ ubuntu@copark.space:~/copark/microservice/TicketService/sql
+scp -r -i ../coparkxyz.pem ./microservice/VehicleService/sql/ ubuntu@copark.space:~/copark/microservice/VehicleService/sql

d38382fbc9aa434ca2ba6de02624c58f359d840e
Atharva Darshan Tawde <atawde@ucsc.edu>
2025-05-31 01:39:31 -0700

    full coverage on Home page ui

--- app/admin/src/app/page.tsx
+++ app/admin/src/app/page.tsx
@@ -30,8 +30,6 @@ export default function Page() {
         return <Box>Generate Reports Component</Box>;
       case 'zones':
         return <ManageZones />;
-      default:
-        return <Box>Welcome to Admin Dashboard</Box>;
     }
   };
 

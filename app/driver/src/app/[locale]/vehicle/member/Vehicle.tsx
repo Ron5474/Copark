ea80862f059c14f4ba2e17db58c84f0d12f3a95c
Bryant Oliver <bcoliver@ucsc.edu>
2025-06-01 20:10:34 -0700

    Fixed pipeline build issue

--- app/driver/src/app/[locale]/vehicle/member/Vehicle.tsx
+++ app/driver/src/app/[locale]/vehicle/member/Vehicle.tsx
@@ -56,13 +56,13 @@ export default function MemberVehicles({ isCheckout = false }: { isCheckout?: bo
         vid = res.id
         if (isCheckout) {
           setSelectedVID(vid)
-          setVehicle(fetchedVehicles.find((v: Vehicle) => v.id === vid))
+          setVehicle((fetchedVehicles ?? []).find((v: Vehicle) => v.id === vid))
         }
       } else {
         vid = null
       }
     })()
-  }, [setSelectedVID, isCheckout])
+  }, [setSelectedVID, isCheckout, setVehicle])
 
   const handleAddOpen = () => setAddOpen(true)
   const handleAddClose = () => setAddOpen(false)

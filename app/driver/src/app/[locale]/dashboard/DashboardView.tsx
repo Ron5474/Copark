6e26ddb1efa0ef55b7eb335b7bf230a651cbc9b6
Bryant Oliver <bcoliver@ucsc.edu>
2025-06-02 18:07:03 -0700

    Fixed ui durationType display translation

--- app/driver/src/app/[locale]/dashboard/DashboardView.tsx
+++ app/driver/src/app/[locale]/dashboard/DashboardView.tsx
@@ -169,7 +169,7 @@ export default function DashboardView() {
                 }}
               >
                 <Typography fontWeight="bold" color="primary.main">
-                  {permit.durationType.charAt(0).toUpperCase() + permit.durationType.slice(1) + " " + (permit.area === "ANY" ? t("allLotsAccess") : permit.area)}
+                  {/*permit.durationType.charAt(0).toUpperCase() + permit.durationType.slice(1)*/ tp(permit.durationType) + " " + (permit.area === "ANY" ? t("allLotsAccess") : permit.area)}
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
                   {t("active")}: {new Date(permit.activeDate!).toLocaleDateString()}

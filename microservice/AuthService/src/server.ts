8972b632093c15419e0aff7bc2c589f55d114130
Swayam Shah <sshah36@ucsc.edu>
2025-06-05 16:31:53 -0700

    fix the test breaking

--- microservice/AuthService/src/server.ts
+++ microservice/AuthService/src/server.ts
@@ -7,15 +7,8 @@ const p = resolve(__dirname, isBuilt ? '../../../../.prod.env': '../../../.env')
 dotenv.config({ path: p})
 
 
-try {
-  (async () => {
-  const { default: app } = await import("./app");
-  
+import app from './app';
   app.listen(3010, () => {
     console.log(`Server Running on port 3010`)
     console.log('API Testing UI: http://localhost:3010/api/v0/docs/')
   })
-})();
-} catch (error) {
-  console.log(error)
-}
